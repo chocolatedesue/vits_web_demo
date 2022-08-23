18de7c80c1ef2065c39dc79dbfc5d42061e554ef
ccds <1356708169@qq.com>
2022-08-23 13:32:31 +0800

    add sg

---
+++ extract_w.py
@@ -0,0 +1,21 @@
+from tabnanny import check
+import torch
+
+file_path = ""
+out_path = file_path[:-4]+'_demo'+file_path[-4:]
+
+# with open (file_path,'r',encoding='uft-8') as f1:
+#     with open (,'w',encoding=  'uft-8') as f2:
+        # model = torch.load(file)
+checkpoint = torch.load(file_path)
+
+state_file = checkpoint['model']
+iteration = checkpoint['iteration']
+
+
+torch.save({'model': state_file,
+              'iteration': iteration,
+              'optimizer': None,
+              'learning_rate': None}, file_path)
+
+
