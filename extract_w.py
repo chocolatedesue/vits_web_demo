0bfb14115431188cb538bcbfa3f77ff0a58cfead
ccds <1356708169@qq.com>
2022-08-25 21:15:08 +0800

    fix extractor

--- extract_w.py
+++ extract_w.py
@@ -7,7 +7,7 @@ out_path = file_path[:-4]+'_demo'+file_path[-4:]
 # with open (file_path,'r',encoding='uft-8') as f1:
 #     with open (,'w',encoding=  'uft-8') as f2:
         # model = torch.load(file)
-checkpoint = torch.load(file_path)
+checkpoint = torch.load(file_path,map_location='cpu')
 
 state_file = checkpoint['model']
 iteration = checkpoint['iteration']
