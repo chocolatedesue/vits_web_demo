1d1c17cc7373437cc03b4a422d29a78370366b68
ccds <1356708169@qq.com>
2022-08-30 12:55:32 +0800

    fix config

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ from text import text_to_sequence
 from mel_processing import spectrogram_torch
 import argparse
 
-def get_text(text):
-    text_norm = text_to_sequence(text,  hps.data.text_cleaners)
+def get_text(text, hps, is_phoneme=False):
+    text_norm = text_to_sequence(text, hps.symbols, [] if is_phoneme else hps.data.text_cleaners)
     if hps.data.add_blank:
         text_norm = commons.intersperse(text_norm, 0)
-    text_norm = LongTensor(text_norm).to(device)
+    text_norm = LongTensor(text_norm)
     return text_norm
 
 
