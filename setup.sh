22ffb6d3fed57c0ebd891c29b953d159aaaef7e0
maloneu72ab <121898642+maloneu72ab@users.noreply.github.com>
2023-01-09 09:26:59 +0000

    remove slider

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
-sudo su 
+# sudo su 
+conda create -n dl  python=3.9 -y
+conda init bash
+bash
 
+conda activate dl
+export POETRY_VERSION=1.3.1
 export DEBIAN_FRONTEND=noninteractive && \
-    apt-get update && \
-    apt-get install cmake build-essential  -y --no-install-recommends && \
+    sudo apt-get update && \
+    sudo apt-get install cmake build-essential  -y --no-install-recommends && \
     pip install poetry==$POETRY_VERSION 
 
-export POETRY_VERSION=1.3.1
-pip install poetry==$POETRY_VERSION  && \
+
 poetry export -f requirements.txt -o requirements.txt --without dev --without test  --without-hashes  && \
 pip install --upgrade pip  && \
 pip install -r requirements.txt  
